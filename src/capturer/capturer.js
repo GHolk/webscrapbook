GHolk/webscrapbook

commit cd3afef949722692cc695b64f59f6bf2ae072562

capture: fix potential breakage of download folder detection (#249)

src/capturer/capturer.js
@@ -218,13 +218,8 @@
         const url = URL.createObjectURL(blob);
         const prefix = options["capture.saveFolder"] + "/" + (dir ? dir + '/' : '');
         isFilenameTaken = async (path) => {
-          const id = await browser.downloads.download({
-            url,
-            filename: prefix + path,
-            conflictAction: "uniquify",
-            saveAs: false,
-          });
-          const newFilename = await new Promise((resolve, reject) => {
+          const newFilename = await new Promise(async (resolve, reject) => {
+            let id = null;
             const onChanged = async (delta) => {
               if (delta.id !== id) { return; }
               if (delta.filename) {
@@ -248,6 +243,12 @@
               }
             };
             browser.downloads.onChanged.addListener(onChanged);
+            id = await browser.downloads.download({
+              url,
+              filename: prefix + path,
+              conflictAction: "uniquify",
+              saveAs: false,
+            });
           });
           return scrapbook.filepathParts(newFilename)[1] !== path;
         };

commit 6001499c9800def74aca0799946731f670ec2294

Retry after 1s if fail to remove in filename test
Sometimes Firefox fails to remove the downloaded file
after the complete event and throw a "file does not exist" error.
This caused capturing hanged in #249.

Though this should be Firefox's bug, mozilla may not fix it very quickly,
so we handle it in extension level.
This commit will wait 1s and retry removing the file 5 times.

src/capturer/capturer.js
@@ -233,7 +233,23 @@
                   browser.downloads.onChanged.removeListener(onChanged);
                   const [item] = await browser.downloads.search({id});
                   const filename = item.filename;
-                  if (item.exists) { await browser.downloads.removeFile(id); }
+                  if (item.exists) {
+                    for (let i=0; true; i++) {
+                      try {
+                        console.log(`removing file: ${id} ${item.filename}`);
+                        await browser.downloads.removeFile(id);
+                        break;
+                      }
+                      catch (error) {
+                        const msg = `fail to remove ${item.filename}\n${error}`;
+                        console.log(msg);
+                        if (i == 5) throw error;
+                        console.log(`retry after 1s`);
+                        await sleep(1);
+                        continue;
+                      }
+                    }
+                  }
                   await browser.downloads.erase({id});
                   resolve(filename);
                 } else if (delta.error) {